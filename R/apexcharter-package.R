f699021a6c63dcb821403d930b8464418286e638
pvictor <perrier.victor@gmail.com>
2018-08-28 15:30:59 +0200

    update ApexCharts to 1.3.14 & import pipe

---
+++ R/apexcharter-package.R
@@ -0,0 +1,25 @@
+#' An \code{htmlwidget} interface to the
+#' ApexCharts javascript chart library
+#' 
+#' This package allow you to use ApexCharts.js (\url{https://apexcharts.com/}),
+#' to create interactive and modern SVG charts.
+#' 
+#' @name apexcharter-package
+#' @docType package
+#' @author Victor Perrier (@@dreamRs_fr)
+NULL
+
+#' apexcharter exported operators and S3 methods
+#' 
+#' The following functions are imported and then re-exported
+#' from the apexcharter package to avoid listing the magrittr
+#' as Depends of apexcharter
+#' 
+#' @name apexcharter-exports
+NULL
+
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @export
+#' @rdname apexcharter-exports
+NULL
