7143c6d378cac4fab84ccd8c6e546f674187018e
pvictor <perrier.victor@gmail.com>
2018-12-18 18:02:09 +0100

    update ApexCharts to 2.4.2

--- R/parse-data.R
+++ R/parse-data.R
@@ -4,12 +4,16 @@
 #' @description Convert data to a format suitable for ApexCharts.js
 #'
 #' @param data A \code{data.frame} or an object coercible to \code{data.frame}.
+#' @param add_names Use names of columns in output. Can be logical to
+#'  reuse \code{data} names or a character vector of new names.
 #'
 #' @export
 #' @importFrom data.table transpose
+#' @importFrom stats setNames
 #'
-parse_df <- function(data) {
+parse_df <- function(data, add_names = FALSE) {
   data <- as.data.frame(data)
+  names_ <- names(data)
   l <- lapply(
     X = data[],
     FUN = function(x) {
@@ -24,5 +28,14 @@ parse_df <- function(data) {
       }
     }
   )
-  data.table::transpose(l)
+  ll <- data.table::transpose(l)
+  if (isTRUE(add_names)) {
+    ll <- lapply(ll, as.list)
+    ll <- lapply(ll, setNames, nm = names_)
+  }
+  if (is.character(add_names) & length(add_names) == length(names_)) {
+    ll <- lapply(ll, as.list)
+    ll <- lapply(ll, setNames, nm = add_names)
+  }
+  return(ll)
 }
