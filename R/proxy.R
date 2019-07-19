1c41b14a2f2a7b2564728d0ec9b99cf5a7385dac
pvictor <perrier.victor@gmail.com>
2019-07-19 16:00:10 +0200

    update options with proxy + examples

--- R/proxy.R
+++ R/proxy.R
@@ -73,23 +73,145 @@ apexchartProxy <- function(shinyId, session = shiny::getDefaultReactiveDomain())
 #' @export
 #'
 #' @examples
-#' \dontrun{
 #' 
 #' if (interactive()) {
-#' 
-#' 
-#' 
+#'   library(shiny)
+#'   
+#'   ui <- fluidPage(
+#'     fluidRow(
+#'       column(
+#'         width = 8, offset = 2,
+#'         tags$h2("Real time chart"),
+#'         apexchartOutput(outputId = "chart")
+#'       )
+#'     )
+#'   )
+#'   
+#'   server <- function(input, output, session) {
+#'     
+#'     rv <- reactiveValues()
+#'     rv$df <- data.frame(
+#'       date = Sys.Date() + 1:20,
+#'       values = sample(10:90, 20, TRUE)
+#'     )
+#'     
+#'     observe({
+#'       invalidateLater(1000, session)
+#'       df <- isolate(rv$df)
+#'       # Append new line of data
+#'       df <- rbind(
+#'         df, data.frame(
+#'           date = df$date[length(df$date)] + 1,
+#'           values = sample(10:90, 1, TRUE)
+#'         )
+#'       )
+#'       rv$df <- df
+#'     })
+#'     
+#'     output$chart <- renderApexchart({
+#'       # Generate chart once
+#'       apex(isolate(rv$df), aes(date, values), "spline") %>% 
+#'         ax_xaxis(
+#'           range = 10 * 24 * 60 * 60 * 1000
+#'           # Fixed range for x-axis : 10 days
+#'           # days*hours*minutes*seconds*milliseconds
+#'         )
+#'     })
+#'     
+#'     observe({
+#'       # Update chart to add new data
+#'       apexchartProxy("chart") %>%
+#'         ax_proxy_series(
+#'           parse_df(rv$df),
+#'           T
+#'         )
+#'     })
+#'     
+#'   }
+#'   
+#'   shinyApp(ui, server)
 #' }
 #' 
-#' }
 ax_proxy_series <- function(proxy, newSeries, animate = TRUE) {
   .ax_proxy2(
     proxy = proxy, 
     name = "series", 
-    l = list(newSeries = list(newSeries), animate = animate)
+    l = list(newSeries = newSeries, animate = animate)
   )
 }
 
+
+
+#' @title Proxy for updating options 
+#' 
+#' @description Allows you to update the configuration object.
+#'
+#' @param proxy A \code{apexchartProxy} \code{htmlwidget} object.
+#' @param options New options to set.
+#'
+#' @export
+#'
+#' @examples
+#' 
+#' if (interactive()) {
+#'   library(shiny)
+#'   
+#'   ui <- fluidPage(
+#'     fluidRow(
+#'       column(
+#'         width = 8, offset = 2,
+#'         tags$h2("Update options"),
+#'         apexchartOutput(outputId = "chart"),
+#'         checkboxInput(
+#'           inputId = "show_label_xaxis",
+#'           label = "Show x-axis labels"
+#'         ),
+#'         textInput(
+#'           inputId = "yaxis_title",
+#'           label = "Y-axis title"
+#'         )
+#'       )
+#'     )
+#'   )
+#'   server <- function(input, output, session) {
+#'     
+#'     output$chart <- renderApexchart({
+#'       apexchart() %>%
+#'         ax_chart(type = "bar") %>% 
+#'         ax_series(list(
+#'           name = "Example",
+#'           data = c(23, 43, 76, 31)
+#'         )) %>% 
+#'         ax_xaxis(
+#'           categories = c("Label A", "Label B",
+#'                          "Label C", "Label D")
+#'         )
+#'     })
+#'     
+#'     observe({
+#'       apexchartProxy("chart") %>%
+#'         ax_proxy_options(list(
+#'           xaxis = list(
+#'             labels = list(show = input$show_label_xaxis)
+#'           ),
+#'           yaxis = list(
+#'             title = list(text = input$yaxis_title)
+#'           )
+#'         ))
+#'     })
+#'     
+#'   }
+#'   
+#'   shinyApp(ui, server)
+#' }
+#' 
+ax_proxy_options <- function(proxy, options) {
+  .ax_proxy2(
+    proxy = proxy, 
+    name = "options", 
+    l = list(options = options)
+  )
+}
 
 
 
